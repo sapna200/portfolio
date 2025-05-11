5a1f144777869e525acf49d0f8eadbea82c18e6d
sapna200 <sapnasarkar192001@gmail.com>
2025-05-11 21:09:09 +0530

    remove unwanted imports

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  SiLinux,
   SiVisualstudiocode,
   SiPostman,
-  SiAndroidstudio,
-  SiIntellijidea,
   SiFigma,
 
 } from "react-icons/si";
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,7 +8,6 @@ import { CgGitFork } from "react-icons/cg";
 
 import {
   AiFillStar,
-  AiOutlineStar,
   AiOutlineHome,
   AiOutlineFundProjectionScreen,
   AiOutlineUser,
